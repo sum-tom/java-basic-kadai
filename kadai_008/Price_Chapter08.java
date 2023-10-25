04838b6b034154dee6e417d424175f59996000ac
sum-tom <144065389+sum-tom@users.noreply.github.com>
2023-10-25 23:50:12 +0900

    Add files via upload

---
+++ kadai_008/Price_Chapter08.java
@@ -0,0 +1,51 @@
+package kadai_008;
+
+public class Price_Chapter08 {
+	public static void main(String[] args) {
+	
+	int userAge = 30;
+	int serviceCost ;
+	
+	switch(userAge) {
+	   case 10  -> {
+		   			serviceCost=1000;
+		   			System.out.println(userAge + "代の料金は"+ serviceCost +"円");
+	   				}
+	   case 20  -> {
+  			serviceCost=2000;
+  			System.out.println(userAge + "代の料金は"+ serviceCost +"円");
+				}
+	   case 30  -> {
+  			serviceCost=3000;
+  			System.out.println(userAge + "代の料金は"+ serviceCost +"円");
+				}
+	   case 40  -> {
+  			serviceCost=4000;
+  			System.out.println(userAge + "代の料金は"+ serviceCost +"円");
+				}
+	   case 50  -> {
+  			serviceCost=5000;
+  			System.out.println(userAge + "代の料金は"+ serviceCost +"円");
+				}
+	   case 60  -> {
+  			serviceCost=6000;
+  			System.out.println(userAge + "代の料金は"+ serviceCost +"円");
+				}
+	   case 70  -> {
+  			serviceCost=7000;
+  			System.out.println(userAge + "代の料金は"+ serviceCost +"円");
+				}
+	   case 80  -> {
+  			serviceCost=8000;
+  			System.out.println(userAge + "代の料金は"+ serviceCost +"円");
+				}
+	   default -> {
+ 			serviceCost=500;
+ 			System.out.println(userAge + "代の料金は"+ serviceCost +"円");
+				}
+	   }
+	
+	
+
+	}
+}
